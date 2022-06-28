a8dfd308f376470a34e54c22d514c7aa5a63323d
Chris L <chris.ladden@gmail.com>
2022-06-28 15:12:26 -0500

    created book list api

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+app.use(express.json())
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
@@ -16,6 +17,15 @@ app.use(express.urlencoded({extended: true}))
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Books API!')
+})
+
+// Books
+const booksController = require('./controllers/books_controller.js')
+app.use('/books', booksController)
+
+// 404 PAGE
+app.get('*', (req, res) => {
+  res.send('404')
 })
 
 // LISTEN
